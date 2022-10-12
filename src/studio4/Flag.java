2c26ccebdd56751f3f61fa8ff358440cb4cf1867
samanthalivingston <samanthalivingston@localhost>
2022-10-12 16:39:35 -0400

    studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,29 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+	
+		//drawing background green Rectangle 
+		StdDraw.setPenColor(193, 255, 155);
+		StdDraw.filledRectangle(.5, .5, .3, 0.2);
 		
+		//outine the rectangle
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.rectangle(.5, .5, .3, 0.2);
+		
+		// making the circles
+		StdDraw.setPenColor(100, 0, 100);
+		StdDraw.filledCircle(.5, .6, .05);
+		
+		StdDraw.setPenColor(100, 0, 100);
+		StdDraw.filledCircle(.3, .6, .05);
+		
+		StdDraw.setPenColor(100, 0, 100);
+		StdDraw.filledCircle(.7, .6, .05);
+		
+		//making the elipses
+		StdDraw.setPenColor(0, 39, 70);
+		StdDraw.ellipse(.5, .4, .05, 0.08);
+		StdDraw.ellipse(.3, .4, .05, 0.08);
+		StdDraw.ellipse(.7, .4, .05, 0.08);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,22 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shape  = in.next();	
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean filled = in.nextBoolean();
+		
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double halfWidth = in.nextDouble();
+		double halfHeight = in.nextDouble();
+		
+		//draw rectangle
+		StdDraw.setPenColor(red, green, blue);
+		StdDraw.rectangle(x,y,halfWidth, halfHeight);
+		
+		
+		
 	}
 }
